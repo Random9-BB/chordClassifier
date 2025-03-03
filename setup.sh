f27931e752eeee5281824fcb61dff96fed9401d4
jinbaibei30 <jinbaibei30@163.com>
2025-03-03 15:40:04 +0800

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 curl -L -o ./dataset/guitar-chords-v3.zip\
   https://www.kaggle.com/api/v1/datasets/download/fabianavinci/guitar-chords-v3
 
-unzip -o ./dataset/guitar-chords-v3.zip -d ./Dataset
-rm ./Dataset/guitar-chords-v3.zip
+unzip -o ./dataset/guitar-chords-v3.zip -d ./dataset
+rm ./dataset/guitar-chords-v3.zip
